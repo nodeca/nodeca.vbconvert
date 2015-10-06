45122ff669e7c42e0deb325985f0b7ff2e483097
Alex Kocharin <alex@kocharin.ru>
2015-10-06 15:48:21 +0000

    Convert topics and posts stub

--- cli/lib/posts.js
+++ cli/lib/posts.js
@@ -4,7 +4,89 @@
 'use strict';
 
 
+var async    = require('async');
+var mongoose = require('mongoose');
+
+
 module.exports = function (N, callback) {
-  N.logger.info('Post conversion finished');
+  /* not ready yet */
+  /* eslint-disable no-unreachable */
   callback();
+  return;
+
+  /* eslint-disable max-nested-callbacks */
+  N.vbconvert.getConnection(function (err, conn) {
+    if (err) {
+      callback(err);
+      return;
+    }
+
+    conn.query('SELECT threadid FROM thread ORDER BY threadid ASC', function (err, threads) {
+      if (err) {
+        conn.release();
+        callback(err);
+        return;
+      }
+
+      async.eachSeries(threads, function (thread, next) {
+        N.models.forum.Topic.findOne({ hid: thread.threadid })
+            .select('_id')
+            .lean(true)
+            .exec(function (err, topic) {
+
+          if (err) {
+            next(err);
+            return;
+          }
+
+          conn.query('SELECT pagetext,dateline,ipaddress FROM post ' +
+              'WHERE threadid = ? ORDER BY postid ASC',
+              [ thread.threadid ],
+              function (err, rows) {
+
+            if (err) {
+              next(err);
+              return;
+            }
+
+            if (rows.length === 0) {
+              next();
+              return;
+            }
+
+            N.models.forum.Post.find({ topic: topic._id }).remove().exec(function (err) {
+              if (err) {
+                next(err);
+                return;
+              }
+
+              var bulk = N.models.forum.Post.collection.initializeOrderedBulkOp();
+
+              rows.forEach(function (row, i) {
+                bulk.insert({
+                  _id:    new mongoose.Types.ObjectId(row.dateline),
+                  topic:  topic._id,
+                  hid:    i + 1,
+                  html:   row.pagetext,
+                  ip:     row.ipaddress
+                });
+              });
+
+              bulk.execute(next);
+            });
+          });
+        });
+      }, function (err) {
+        if (err) {
+          conn.release();
+          callback(err);
+          return;
+        }
+
+        conn.release();
+        N.logger.info('Post conversion finished');
+        callback();
+      });
+    });
+  });
 };
--- cli/lib/topics.js
+++ cli/lib/topics.js
@@ -4,7 +4,101 @@
 'use strict';
 
 
+var async    = require('async');
+var mongoose = require('mongoose');
+
+
 module.exports = function (N, callback) {
-  N.logger.info('Topic conversion finished');
+  /* not ready yet */
+  /* eslint-disable no-unreachable */
   callback();
+  return;
+
+  var maxid = 0;
+
+  /* eslint-disable max-nested-callbacks */
+  N.vbconvert.getConnection(function (err, conn) {
+    if (err) {
+      callback(err);
+      return;
+    }
+
+    conn.query('SELECT forumid FROM forum ORDER BY forumid ASC', function (err, forums) {
+      if (err) {
+        conn.release();
+        callback(err);
+        return;
+      }
+
+      async.eachSeries(forums, function (forum, next) {
+        N.models.forum.Section.findOne({ hid: forum.forumid })
+            .lean(true)
+            .exec(function (err, section) {
+
+          if (err) {
+            next(err);
+            return;
+          }
+
+          conn.query('SELECT threadid,forumid,title,dateline FROM thread ' +
+              'WHERE forumid = ? ORDER BY threadid ASC',
+              [ forum.forumid ],
+              function (err, rows) {
+
+            if (err) {
+              next(err);
+              return;
+            }
+
+            if (rows.length === 0) {
+              next();
+              return;
+            }
+
+            var bulk = N.models.forum.Topic.collection.initializeUnorderedBulkOp();
+
+            rows.forEach(function (row) {
+              if (maxid < row.threadid) { maxid = row.threadid; }
+
+              bulk.find({ hid: row.threadid }).upsert().update({
+                $setOnInsert: {
+                  _id:      new mongoose.Types.ObjectId(row.dateline),
+                  title:    row.title,
+                  hid:      row.threadid,
+                  section:  section._id,
+                  st:       N.models.forum.Topic.statuses.OPEN,
+                  cache:    {},
+                  cache_hb: {}
+                }
+              });
+            });
+
+            bulk.execute(next);
+          });
+        });
+      }, function (err) {
+        if (err) {
+          conn.release();
+          callback(err);
+          return;
+        }
+
+        N.models.core.Increment.update(
+          { key: 'topic' },
+          { $set: { value: maxid } },
+          { upsert: true },
+          function (err) {
+            if (err) {
+              callback(err);
+              return;
+            }
+
+            conn.release();
+            N.logger.info('Topic conversion finished');
+            callback();
+          }
+        );
+      });
+    });
+  });
 };
