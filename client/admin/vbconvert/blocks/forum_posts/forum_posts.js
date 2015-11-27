2ee3241a247e48951e262717cd57dadff185a434
Vitaly Puzrin <vitaly@rcdesign.ru>
2015-11-27 03:30:59 +0300

    Stubbed live update for progress bar

--- client/admin/vbconvert/blocks/forum_posts/forum_posts.js
+++ client/admin/vbconvert/blocks/forum_posts/forum_posts.js
@@ -1,7 +1,7 @@
 // Display post import progress in admin interface
 //
-
 'use strict';
+
 
 var ko = require('knockout');
 
@@ -11,16 +11,16 @@ var view = null;
 var SELECTOR = '#vbconvert-task-forum-posts';
 
 
-/*function update_task_status(task_info) {
+function update_task_status(task_info) {
   if (!view) { return; }
 
-  view.started(task_info.started);
-  view.current(task_info.current);
-  view.total(task_info.total);
-}*/
+  // view.started(task_info.started);
+  view.current(task_info.chunks.done + task_info.chunks.errored);
+  view.total(task_info.chunks.pending);
+}
 
 
-N.wire.on('navigate.done', function vbconvert_forum_post_task_widget_setup() {
+N.wire.on('navigate.done:admin.vbconvert.index', function vbconvert_forum_post_task_widget_setup() {
   if (!$(SELECTOR).length) { return; }
 
   view = {
@@ -31,28 +31,30 @@ N.wire.on('navigate.done', function vbconvert_forum_post_task_widget_setup() {
 
   ko.applyBindings(view, $(SELECTOR)[0]);
 
-  // N.live.on('admin.rebuild.forum_post', update_task_status)
+  N.live.on('admin.vbconvert.forum_posts', update_task_status);
 });
 
 
-N.wire.on('navigate.exit', function vbconvert_forum_post_task_widget_teardown() {
+N.wire.on('navigate.exit:admin.vbconvert.index', function vbconvert_forum_post_task_widget_teardown() {
   if (!$(SELECTOR).length) { return; }
 
   view = null;
   ko.cleanNode($(SELECTOR)[0]);
 
-  // N.live.off('admin.rebuild.forum_post', update_task_status);
+  N.live.off('admin.vbconvert.forum_posts', update_task_status);
 });
 
 
-N.wire.once('navigate.done', function vbconvert_forum_post_task_widget_setup_handlers() {
+N.wire.once('navigate.done:admin.vbconvert.index', function vbconvert_forum_post_task_widget_setup_handlers() {
+
   // Click on "start" button
   //
   N.wire.on(module.apiPath + '.start', function vbconvert_start() {
     N.io.rpc(module.apiPath + '.start')
       .done(function () {
         // TODO
-        N.wire.emit('navigate.reload');
+        // N.wire.emit('navigate.reload');
+        view.started(true);
       });
   });
 
@@ -63,7 +65,8 @@ N.wire.once('navigate.done', function vbconvert_forum_post_task_widget_setup_han
     N.io.rpc(module.apiPath + '.stop')
       .done(function () {
         // TODO
-        N.wire.emit('navigate.reload');
+        // N.wire.emit('navigate.reload');
+        view.started(false);
       });
   });
 });
