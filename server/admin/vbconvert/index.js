cddcf0f1e6df0bb93fb2f8154210bbe1d8a224a2
Alex Kocharin <alex@kocharin.ru>
2015-11-27 10:39:07 +0000

    Fix multiple UI issues

--- server/admin/vbconvert/index.js
+++ server/admin/vbconvert/index.js
@@ -6,6 +6,7 @@ module.exports = function (N, apiPath) {
   N.validate(apiPath, {});
 
   N.wire.on(apiPath, function vbconvert_index(env) {
+    env.res.head.title = env.t('title');
     env.res.blocks = [];
   });
 };
