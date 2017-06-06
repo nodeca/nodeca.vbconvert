e4ca630b42599a0e46f9f57d40a8b3cfe4bc821e
Alex Kocharin <alex@kocharin.ru>
2017-06-06 17:58:08 +0000

    Remove revision from posts on import
    This reverts commit 87331093895b278abea4d432d522b0fa41851ef4.

--- cli/_lib/topics.js
+++ cli/_lib/topics.js
@@ -196,7 +196,6 @@ module.exports = Promise.coroutine(function* (N) {
           ts,
           md:         post.pagetext,
           html:       '<p>' + _.escape(post.pagetext) + '</p>',
-          revision:   0,
           ip:         post.ipaddress,
           params_ref: params_id,
           attach:     [] // an array in DB is required by parser
